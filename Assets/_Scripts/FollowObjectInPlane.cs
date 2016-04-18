55b0814f84e16916b447e263a6b4846735186e41
David Garia Centelles <David.GarciaC@e-campus.uab.cat>
2016-04-18 22:56:36 +0200

    Version 7: Added Pickup generation, Score and Time Limit

---
+++ Assets/_Scripts/FollowObjectInPlane.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObjectInPlane : MonoBehaviour {
+
+	public GameObject followedObject;
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - followedObject.transform.position;
+	}
+
+	void Update () {
+
+		if (followedObject != null) {
+			transform.position = new Vector3 (
+				followedObject.transform.position.x + offset.x, 
+				offset.y, 
+				followedObject.transform.position.z + offset.z);
+
+			transform.rotation = followedObject.transform.rotation;
+		}
+	}
+}
---
+++ Assets/_Scripts/UIController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	//Player UI
+	public Text info_energy;
+	public Text info_lifes;
+	public Text info_speed;
+	public Text score_number;
+	public Text timer_number;
+	public Text pickup_info;
+	public Text spaceship_info;
+	public Slider energy_slider;
+
+	//Camera UI
+	public Text info_camera;
+
+	//Game UI
+	public Text game_info;
+
+	public void refreshPlayerInfo (float current_energy, float current_number_of_lifes, float current_speed) {
+		info_energy.text = "Energy: " + ((int) current_energy).ToString() + "%";
+		info_lifes.text = "Lifes: " + current_number_of_lifes.ToString ();
+		info_speed.text = "Speed: " + ((int) current_speed).ToString ();
+		energy_slider.normalizedValue = (((int) current_energy) / 100.0f);
+	}
+
+	public void refreshCameraInfo (string info) {
+		info_camera.text = info;
+	}
+
+	public void refreshGameInfo (string info) {
+		game_info.text = info;
+	}
+
+	public void refreshScore (int score) {
+		score_number.text = score.ToString();
+	}
+
+	public void refreshPickupPosition (int Z, int X) {
+		pickup_info.text = "Pickup (X,Z) = (" + X.ToString () + "," + Z.ToString () + ")"; 
+	}
+
+	public void refreshSpaceshipPosition (int Z, int X) {
+		spaceship_info.text = "Spaceship (X,Z) = (" + X.ToString () + "," + Z.ToString () + ")"; 
+	}
+
+	public void refreshTimer (int timer) {
+
+		timer_number.text = timer.ToString ();
+
+	}
+}
